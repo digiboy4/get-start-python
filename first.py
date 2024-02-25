2d30e90a70b5d149a2f605580a3973c35cece0ae
digital boy <161239858+digiboy4@users.noreply.github.com>
2024-02-25 17:50:17 +0000

    Create first.py

---
+++ first.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+"""
+Getting Started with Python:
+Variables and Data Structures
+"""
+
+# Basic data types
+# ----------------
+
+# string
+nation = 'United States'
+
+# integer
+year = 2023
+
+# decimal
+birth_rate = 18.4
+
+# Variables of different types can be
+# concatenated if cast as strings
+sentence = 'The ' + nation + ' had a birth rate of ' + str(birth_rate) + \
+    ' per thousand population in ' + str(year) + '.'
+print(sentence)
+
+
+# Data structures
+# -----------------
+
+
+# Lists (are mutable)
+
+# Create a list:
+car_list = ['Honda', 'Ford', 'Buick']
+
+# Retrieve the first item:
+print(car_list[0])
+
+# Sort the list, then print
+car_list.sort()
+print(car_list)
+
+# Retrieve the first two items (slicing)
+print(car_list[0:2])
+
+# Add and remove items from the list
+car_list.append('Kia')
+car_list.remove('Honda')
+print(car_list)
+
+
+# Tuples (are immutable)
+
+# Create a tuple
+cars_tuple = ('Honda', 'Ford', 'Buick')
+
+# Retrieve the third item (note zero index)
+print(cars_tuple[2])
+
+
+# Dictionaries
+
+# Create a dictionary
+cars_dict = {'make': 'Honda', 'year': 2010, 'color': 'Silver'}
+
+# Retrieve the value for the key 'year'
+print(cars_dict['year'])
+
+# Set a new value for the key 'year'
+cars_dict['year'] = 2013
+print(cars_dict['year'])
